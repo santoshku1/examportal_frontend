25769cab928abd73c34be56b6261c23168d38d32
Santosh Kumar <your_email@example.com>
2022-10-21 15:23:41 +0530

    connecting jwt spring security   client to server

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    username: '',
+    password: ''
+  };
 
-  ngOnInit(): void {
+
+  constructor(private snack: MatSnackBar, private login: LoginService) { }
+
+  ngOnInit(): void { }
+
+  formSubmit() {
+    console.log("login form clicked")
+    if (this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.snack.open("User name is required", "", {
+        duration: 2000,
+      });
+      return;
+    }
+    if (this.loginData.password.trim() == '' || this.loginData.password == null) {
+      this.snack.open("Password is required", "", {
+        duration: 2000,
+      });
+      return;
+    }
+
+    //request to server to generate token
+
+    this.login.generateToken(this.loginData).subscribe(
+      (data: any) => {
+          console.log("success");
+          console.log(data);
+      },
+      (error) => {
+        console.log("error");
+        console.log(error);
+    }
+
+    )
+
   }
 
 }
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  //generate token
+  public generateToken(loginData: any) {
+
+    return this.http.post(`${baseUrl}/generate-token`, loginData)
+
+  }
+
+  
+
+}
